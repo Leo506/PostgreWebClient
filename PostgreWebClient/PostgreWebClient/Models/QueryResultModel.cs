9c7de54f909cb35347112fbefd7d69a663e79de4
Artem Levin <leoartemlev@yandex.ru>
2022-10-28 22:26:39 +0500

    added Model fro query results

---
+++ PostgreWebClient/PostgreWebClient/Models/QueryResultModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PostgreWebClient.Models;
+
+public class QueryResultModel
+{
+    public List<int>? TableRows { get; set; }
+
+    public List<object>? Rows { get; set; }
+
+    public bool HasError { get; set; } = false;
+    
+    public string? ErrorText { get; set; }
+}

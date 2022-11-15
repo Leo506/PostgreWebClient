36796336a460da5d0cdcb72d284722e7be59d9d8
Artem Levin <leoartemlev@yandex.ru>
2022-11-15 18:31:46 +0500

    added getting database info on redirect on Manipulation page

--- PostgreWebClient/PostgreWebClient/wwwroot/js/manipulation.js
+++ PostgreWebClient/PostgreWebClient/wwwroot/js/manipulation.js
@@ -6,17 +6,28 @@ let sendButton = document.getElementById("sendButton");
 
 connection.start().then(function () {
     sendButton.disabled = false;
+    getDatabaseInfo();
 }).catch(logError);
 
 connection.on("getTable", createQueryResultTable);
 connection.on("getDatabaseInfo", createDbInfoList);
 
 sendButton.addEventListener("click", function (event) {
+    executeQuery()
+    getDatabaseInfo();
+    event.preventDefault();
+});
+
+
+
+function executeQuery() {
     let queryText = editor.session.getValue();
     connection.invoke("ExecuteQuery", queryText).catch(logError);
+}
+
+function getDatabaseInfo() {
     connection.invoke("GetDatabaseInfo").catch(logError);
-    event.preventDefault();
-})
+}
 
 function logError(err) {
     return console.error(err.toString());

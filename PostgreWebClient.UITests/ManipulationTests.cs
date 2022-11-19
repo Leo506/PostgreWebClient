ca818d305a3a1a66dc8f0ddd374ad87721b0914a
Artem Levin <leoartemlev@yandex.ru>
2022-11-19 23:59:56 +0500

    remove ui tests

--- PostgreWebClient.UITests/ManipulationTests.cs
+++ PostgreWebClient.UITests/ManipulationTests.cs
@@ -1,29 +1,31 @@
-using FluentAssertions;
+﻿using FluentAssertions;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace PostgreWebClient.UITests;
 
-public class ConnectionTests : TestClassBase
+public class ManipulationTests : TestClassBase
 {
-    public ConnectionTests() : base(new ChromeDriver())
+    public ManipulationTests() : base(new ChromeDriver())
     {
     }
 
     [Fact]
-    public void ConnectToDatabase_AllGood_ReturnsManipulationView()
+    public void OnManipulationPage_ThereAreSchemaList()
     {
         // arrange
+        // connect to database
         _driver.Navigate().GoToUrl("https://localhost:7108/Connection");
-
-        // act
         _driver.FindElement(By.Id("UserId")).SendKeys(ConnectionConstants.UserId);
         _driver.FindElement(By.Id("Password")).SendKeys(ConnectionConstants.Password);
         _driver.FindElement(By.Id("Database")).SendKeys(ConnectionConstants.Database);
         _driver.FindElement(By.Id("Connect")).Click();
 
+        // act
+        var schemaList = _driver.FindElement(By.Id("databaseInfo")).FindElements(By.XPath(".//*"));
+
         // assert
-        _driver.Url.EndsWith("/manipulation").Should().BeTrue();
+        schemaList.Count.Should().BeGreaterThan(2);
         
         ClearCookie();
     }
---
+++ PostgreWebClient.UITests/TestClassBase.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace PostgreWebClient.UITests;
+
+public class TestClassBase : IDisposable
+{
+    protected readonly IWebDriver _driver;
+
+    public TestClassBase(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    protected void ClearCookie()
+    {
+        _driver.Manage().Cookies.DeleteAllCookies();
+    }
+
+    public void Dispose()
+    {
+        _driver.Quit();
+        _driver.Dispose();
+    }
+}

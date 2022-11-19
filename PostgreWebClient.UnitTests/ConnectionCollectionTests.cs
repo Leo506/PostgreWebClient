8706a00c6a3b213c405289aa4cb601015d654c50
Artem Levin <leoartemlev@yandex.ru>
2022-11-19 23:11:19 +0500

    create ConnectionCollection. It is a special collection that open and close connection with database on Add/Remove operations

---
+++ PostgreWebClient.UnitTests/ConnectionCollectionTests.cs
@@ -0,0 +1,119 @@
+﻿using System.Data;
+using FluentAssertions;
+using Moq;
+using PostgreWebClient.Models;
+
+namespace PostgreWebClient.UnitTests;
+
+public class ConnectionCollectionTests
+{
+    [Fact]
+    public void ConnectionCollection_SuccessfulCreated()
+    {
+        // act
+        var result = new ConnectionCollection();
+
+        // assert
+        result.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ConnectionCollection_CollectionInitializing_Success()
+    {
+        // act
+        var result = new ConnectionCollection()
+        {
+            ["id1"] = new Mock<IDbConnection>().Object,
+            ["id2"] = new Mock<IDbConnection>().Object
+        };
+
+        // assert
+        result.Count.Should().Be(2);
+    }
+
+    [Fact]
+    public void CollectionInitializing_AllGood_InvokeConnectionOpen()
+    {
+        // arrange
+        var connectionMock = new Mock<IDbConnection>();
+
+        // act
+        var sut = new ConnectionCollection()
+        {
+            ["id"] = connectionMock.Object
+        };
+
+        // assert
+        connectionMock.Verify(connection => connection.Open(), Times.Exactly(1));
+    }
+
+    [Fact]
+    public void Add_AllGood_ChangeCount()
+    {
+        // arrange
+        var sut = new ConnectionCollection();
+        var startCount = sut.Count;
+
+        // act
+        sut.Add("id", new Mock<IDbConnection>().Object);
+        var resultCount = sut.Count;
+
+        var result = resultCount - startCount;
+        
+        // assert
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Add_AllGood_InvokeConnectionOpen()
+    {
+        // arrange
+        var connectionMock = new Mock<IDbConnection>();
+
+        var sut = new ConnectionCollection();
+        
+        // act
+        sut.Add("id", connectionMock.Object);
+
+        // assert
+        connectionMock.Verify(connection => connection.Open(), Times.Exactly(1));
+    }
+
+    [Fact]
+    public void RemoveById_AllGood_ChangeCount()
+    {
+        // arrange
+        var sut = new ConnectionCollection()
+        {
+            ["id"] = new Mock<IDbConnection>().Object
+        };
+
+        var startCount = sut.Count;
+
+        // act
+        sut.RemoveById("id");
+
+        var resultCount = sut.Count;
+
+        // assert
+        resultCount.Should().Be(0);
+        (startCount - resultCount).Should().Be(1);
+    }
+
+    [Fact]
+    public void RemoveById_AllGood_CloseConnection()
+    {
+        // arrange
+        var connectionMock = new Mock<IDbConnection>();
+        var sut = new ConnectionCollection()
+        {
+            ["id"] = connectionMock.Object
+        };
+
+        // act
+        sut.RemoveById("id");
+
+        // assert
+        connectionMock.Verify(connection => connection.Close(), Times.Exactly(1));
+    }
+}
---
+++ PostgreWebClient/PostgreWebClient/Models/ConnectionCollection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Data;
+
+namespace PostgreWebClient.Models;
+
+public class ConnectionCollection : IEnumerable<IDbConnection>
+{
+    public int Count => _connections.Count;
+
+    private readonly Dictionary<string, IDbConnection> _connections = new();
+    
+    public void Add(string id, IDbConnection connection)
+    {
+        _connections.Add(id, connection);
+        connection.Open();
+    }
+
+    public void RemoveById(string id)
+    {
+        var connection = _connections[id];
+        connection.Close();
+        _connections.Remove(id);
+    }
+
+    public IDbConnection this[string key]
+    {
+        get => _connections[key];
+        set
+        {
+            _connections.Add(key, value);
+            value.Open();
+        }
+    }
+    
+    public IEnumerator<IDbConnection> GetEnumerator()
+    {
+        return _connections.Values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}

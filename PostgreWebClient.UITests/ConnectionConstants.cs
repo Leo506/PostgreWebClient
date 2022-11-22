e385f85d40a3b36d0f672a70664657195e33ec9b
Artem Levin <leoartemlev@yandex.ru>
2022-11-22 21:55:11 +0500

    removed UI tests (physically)

--- PostgreWebClient.UITests/ConnectionConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PostgreWebClient.UITests;
-
-public class ConnectionConstants
-{
-    public const string UserId = "admin";
-    public const string Password = "password";
-    public const string Database = "Test";
-}

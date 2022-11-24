f49d04698f9b7479d8a30fb8ec9730ae2f626d2a
Artem Levin <leoartemlev@yandex.ru>
2022-11-24 23:36:44 +0500

    created TableExtractor.cs

---
+++ PostgreWebClient.UnitTests/TableExtractorTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Moq;
+using PostgreWebClient.Extractors;
+using PostgreWebClient.Models;
+using PostgreWebClient.UnitTests.FixtureAttributes;
+
+namespace PostgreWebClient.UnitTests;
+
+public class TableExtractorTests
+{
+    [Theory, AutoMoqData]
+    public void ExtractTable_AllGood_ReturnsTable([Frozen] Mock<IDataReader> reader, TableExtractor sut)
+    {
+        // arrange
+        var expectedTable = new Table()
+        {
+            Columns = new List<string>() { "Col1" },
+            Rows = new List<List<object>>()
+            {
+                new() { "Row" }
+            }
+        };
+
+        reader.SetupGet(dataReader => dataReader.FieldCount).Returns(1);
+        reader.Setup(dataReader => dataReader.GetName(0)).Returns("Col1");
+
+        reader.Setup(dataReader => dataReader.Read()).Returns(true)
+            .Callback(() => reader.Setup(r => r.Read()).Returns(false));
+        reader.Setup(dataReader => dataReader[0]).Returns("Row");
+        
+
+        // act
+        var result = sut.ExtractTable(reader.Object);
+
+        // assert
+        result.Equals(expectedTable).Should().BeTrue();
+    }
+
+    [Theory, AutoMoqData]
+    public void ExtractTable_ReaderIsNull_RaiseException(TableExtractor sut)
+    {
+        // assert
+        Assert.Throws<ArgumentNullException>(() => sut.ExtractTable(null));
+    }
+}
---
+++ PostgreWebClient/PostgreWebClient/Extractors/TableExtractor.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using PostgreWebClient.Abstractions;
+using PostgreWebClient.Models;
+
+namespace PostgreWebClient.Extractors;
+
+public class TableExtractor : ITableExtractor
+{
+    public Table ExtractTable(IDataReader? reader)
+    {
+        if (reader is null)
+            throw new ArgumentNullException(nameof(reader));
+        
+        var table = new Table
+        {
+            Columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList(),
+            Rows = new List<List<object>>()
+        };
+
+        while (reader.Read())
+        {
+            table.Rows.Add(Enumerable.Range(0, reader.FieldCount).Select(i => reader[i]).ToList());
+        }
+
+        return table;
+    }
+}
